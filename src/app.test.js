a2ce5c3f56a7699293af86ce80c7fc68254e11f0
Valeriy Rudenko <lonoriel@gmail.com>
2020-11-24 20:39:04 +0300

    Added tests

--- src/app.test.js
+++ src/app.test.js
@@ -6,7 +6,9 @@ function setInputField(matcher, value, query) {
     input.value = value;
 }
 
-function fillTestData({getByLabelText}) { 
+function fillTestData(result) { 
+    const { getByLabelText } = result;
+
     setInputField("Фамилия *", "Иванов", getByLabelText);
     setInputField("Имя *", "Иван", getByLabelText);
     setInputField("Отчество *", "Иванович", getByLabelText);
@@ -27,28 +29,92 @@ function fillTestData({getByLabelText}) {
 it('works', async () => {
     const { container, getByText, getByTestId } = render(App);
     const button = getByText("Отправить заявку на стажировку");
-
   
    //  2 click on   "Направить заявку на стажировку"
     await fireEvent.click(button);
 
-// 3 expect text on page: "Ваша заявка принята" 
+    // 3 expect text on page: "Ваша заявка принята" 
     // expect(component).toHaveTextContent("123")
     expect(container.textContent).toMatch("Спасибо! Ваша заявка на стажировку отправлена");
+    // expect(123).toBe //toEqual
     
 })
 
 
 
 it('validates email format', async () => {
-    const result = render(App); 
-    const { container, getByText, getByLabelText, getByTestId } = result
+    const result = render(App);
+    const { container, getByText, getByLabelText } = result;
     const button = getByText("Отправить заявку на стажировку");
 
     fillTestData(result);
-    setInputField("Email *", "ivan..ivanovisch@mail.foo", getByLabelText);
-
+    setInputField("Email *", "ivan.ivanovisch@mail..foo", getByLabelText);
     await fireEvent.click(button);
 
-    expect(container.textContent).toMatch("Спасибо! Ваша заявка на стажировку отправлена");
-})
+    expect(container.textContent).toMatch("Пожалуйста, заполните все обязательные поля");
+});
+
+it('validates missing email domain name', async () => {
+    const result = render(App);
+    const { container, getByText, getByLabelText } = result;
+    const button = getByText("Отправить заявку на стажировку");
+
+    fillTestData(result);
+    setInputField("Email *", "ivan.ivanovisch@", getByLabelText);
+    await fireEvent.click(button);
+
+    expect(container.textContent).toMatch("Пожалуйста, заполните все обязательные поля");
+});
+
+
+it('validates missing email @', async () => {
+    const result = render(App);
+    const { container, getByText, getByLabelText } = result;
+    const button = getByText("Отправить заявку на стажировку");
+
+    fillTestData(result);
+    setInputField("Email *", "ivan.ivanovischmail.foo", getByLabelText);
+    await fireEvent.click(button);
+
+    expect(container.textContent).toMatch("Пожалуйста, заполните все обязательные поля");
+});
+
+
+it('validates phone number lenght', async () => {
+    const result = render(App);
+    const { container, getByText, getByLabelText } = result;
+    const button = getByText("Отправить заявку на стажировку");
+
+    fillTestData(result);
+    setInputField("Телефон *", "+7(999)888-77-665", getByLabelText);
+    await fireEvent.click(button);
+
+    expect(container.textContent).toMatch("Пожалуйста, заполните все обязательные поля");
+});
+
+
+it('validates phone number letter', async () => {
+    const result = render(App);
+    const { container, getByText, getByLabelText } = result;
+    const button = getByText("Отправить заявку на стажировку");
+
+    fillTestData(result);
+    setInputField("Телефон *", "+7(999)888-77-0O", getByLabelText);
+    await fireEvent.click(button);
+
+    expect(container.textContent).toMatch("Пожалуйста, заполните все обязательные поля");
+});
+
+
+
+it('validates birthday', async () => {
+    const result = render(App);
+    const { container, getByText, getByLabelText } = result;
+    const button = getByText("Отправить заявку на стажировку");
+
+    fillTestData(result);
+    setInputField("Дата рождения *", "32.10.1995", getByLabelText);
+    await fireEvent.click(button);
+
+    expect(container.textContent).toMatch("Пожалуйста, заполните все обязательные поля");
+});
